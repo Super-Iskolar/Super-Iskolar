ff3479e9b51ef7b5f83e57bd88e5599a293a59e5
Miguel Pardiñas <miguelpardinas20@gmail.com>
2022-03-09 14:34:38 +0800

    Sprint2-day1-miguel
    Added grid and Tilemap under floor
    Adjusted pause UI order in layer to be on top
    Added sprite orientation change on x-axis in MoveLeft and MoveRight -> need fix bug of sprite render image not flipping properly
    Added cinemachine package for camera -> need fix pause menu since it has a fixed position on screen
    Added slippery 2d physics material on IskolarSprite

---
+++ Super Iskolar/Assets/Scripts/PencilWeapon.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PencilWeapon : MonoBehaviour
+{
+
+    public Transform FirePencil;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
